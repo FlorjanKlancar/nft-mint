3000ea7e8b90f6ceb796910dec139bbb4f536f1b
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-04-28 18:29:56 +0200

    fixed type bug

--- components/storage/StorageTable.tsx
+++ components/storage/StorageTable.tsx
@@ -123,7 +123,7 @@ function StorageTable({
         <label className="modal-box relative" htmlFor="">
           <h3 className="text-lg font-bold">Edit file {}</h3>
           <p className="py-4">
-            You've been selected for a chance to get one year of subscription to use Wikipedia for
+            You have been selected for a chance to get one year of subscription to use Wikipedia for
             free!
           </p>
         </label>
